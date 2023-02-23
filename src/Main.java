85554f9c67375ec9947ebb173fc144f060839503
Mert Bayats <mertbaytas@gmail.com>
2023-02-23 14:44:42 +0300

    selam

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double ucret=10,km;
+        Scanner oku=new Scanner(System.in);
+
+        System.out.println("Km giriniz: ");
+        km=oku.nextInt();
+
+        if(km<0){
+            System.out.println("Km negatif deger alamaz!");
+            System.exit(1);
+        }
+
+        ucret+=km*2.20;
+
+        if(ucret<20){
+            ucret=20;
+        }
+        System.out.println("Taksimetre ucretiniz = "+ucret);
+
+
+
+
     }
 }
